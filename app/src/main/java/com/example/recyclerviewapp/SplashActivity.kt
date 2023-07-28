d4c95aa0615a8805651df4dd500bcfc969a1bc3b
YadavYashvant <yashvantyadav855@gmail.com>
2023-07-29 01:55:40 +0530

    Added intent-filter

--- app/src/main/java/com/example/recyclerviewapp/SplashActivity.kt
+++ app/src/main/java/com/example/recyclerviewapp/SplashActivity.kt
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.Window
+import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         supportActionBar?.hide()
         Handler(Looper.getMainLooper()).postDelayed({
