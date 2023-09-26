a567de0f64e42cda6b4f4d52a25af411bfe2d1dc
Yashvant Yadav <yashvantyadav855@gmail.com>
2023-09-26 16:09:55 +0530

    fixed many features

--- app/src/main/java/com/example/recyclerviewapp/SplashActivity.kt
+++ app/src/main/java/com/example/recyclerviewapp/SplashActivity.kt
@@ -20,6 +20,6 @@ class SplashActivity : AppCompatActivity() {
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this,MainActivity::class.java))
             finish()
-        },8500)
+        },1500)
     }
 }
